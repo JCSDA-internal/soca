d3c6bce2510d47a21b2983ec9737d02b0a13294b
Rahul Mahajan <aerorahul@users.noreply.github.com>
2018-10-18 21:30:58 -0400

    add FMS definitions;
    Change-Id: I859681566e3e00214fdea235bea4acb66fc3b670

--- cmake/soca_compiler_flags.cmake
+++ cmake/soca_compiler_flags.cmake
@@ -1,10 +1,9 @@
-# (C) Copyright 2009-2016 ECMWF.
-# 
+# (C) Copyright 2018 UCAR.
+#
 # This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
-# In applying this licence, ECMWF does not waive the privileges and immunities 
-# granted to it by virtue of its status as an intergovernmental organisation nor
-# does it submit to any jurisdiction.
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+add_definitions( -Duse_libMPI -Duse_netCDF -DSPMD )
 
 if( NOT CMAKE_BUILD_TYPE MATCHES "Debug" )
   add_definitions( -DNDEBUG )
