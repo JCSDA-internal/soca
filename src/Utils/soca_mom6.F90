24f4a07e6f09619c84f9339022c60738938b536b
Rahul Mahajan <aerorahul@users.noreply.github.com>
2018-10-22 09:55:34 -0400

    remove coupled model deps
    Change-Id: Ic217302e5c5e78f9ea8b33c7c977cbc05b08f3ce

--- src/Utils/soca_mom6.F90
+++ src/Utils/soca_mom6.F90
@@ -4,7 +4,7 @@
 ! This software is licensed under the terms of the Apache Licence Version 2.0
 ! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 !
-! 
+!
 ! ------------------------------------------------------------------------------
 !> Typical geometry/state for ocean/sea-ice models
 !>
@@ -12,37 +12,37 @@
 !>   level 1          ---- Upper snow level            Qsno
 !>     .                        .                       .
 !>     .                        .                       .
-!>     .                        .                       .        
+!>     .                        .                       .
 !>   level nzs        ---- Lower snow level            Qsno
-!>   level nzs+1      ---- Upper ice level             Qice     
+!>   level nzs+1      ---- Upper ice level             Qice
 !>     .                        .                       .
 !>     .                        .                       .
 !>     .                        .                       .
 !>   level nzs+nzi    ---- Upper ice level             Qice
 !>   level nzs+nzi+1  ---- Ice/Ocean interface level   Tfreeze (from S)
-!>   level nzs+nzi+2  ---- Upper level Ocean           SST    
+!>   level nzs+nzi+2  ---- Upper level Ocean           SST
 !>
-!> 
+!>
 module soca_mom6
 
-  use ocean_model_mod,         only: ocean_state_type, ocean_public_type
+  !use ocean_model_mod,         only: ocean_state_type, ocean_public_type
   use time_manager_mod,        only: time_type
   use kinds
   use MOM_grid,                  only : ocean_grid_type
   use MOM_verticalGrid,          only : verticalGrid_type
   use MOM, only : MOM_control_struct
-  
+
   implicit none
   private
 
   public :: soca_geom_init, soca_geom_end, Coupled, soca_field_init, soca_field_end, soca_ice_column
 
-  type soca_ice_column 
+  type soca_ice_column
      integer                        :: ncat ! Number of ice categories
      integer                        :: nzi  ! Number of ice levels
-     integer                        :: nzs  ! Number of snow levels     
+     integer                        :: nzs  ! Number of snow levels
   end type soca_ice_column
-  
+
   type soca_ocn_data_type
      real(kind=kind_real),  pointer :: T(:,:,:)
      real(kind=kind_real),  pointer :: S(:,:,:)
@@ -52,7 +52,7 @@ module soca_mom6
      real(kind=kind_real),  pointer :: H(:,:,:)
   end type soca_ocn_Data_Type
 
-  type soca_ice_data_type 
+  type soca_ice_data_type
      type(soca_ice_column) :: ice_column
      real(kind=kind_real),  pointer :: part_size(:,:,:)
      real(kind=kind_real),  pointer :: h_ice(:,:,:)
@@ -62,16 +62,16 @@ module soca_mom6
      real(kind=kind_real),  pointer :: enth_snow(:,:,:,:)
      real(kind=kind_real),  pointer :: T_skin(:,:,:)
   end type soca_ice_data_type
-  
+
   type Coupled
-     type (soca_ice_data_type)     :: Ice     
+     type (soca_ice_data_type)     :: Ice
      type (soca_ocn_data_type)     :: Ocn
   end type Coupled
 
 contains
 
   ! ------------------------------------------------------------------------------
-  
+
   subroutine soca_geom_init(G, GV, ice_column, c_conf)
 
     use MOM_dyn_horgrid,           only : dyn_horgrid_type, create_dyn_horgrid, destroy_dyn_horgrid
@@ -92,7 +92,7 @@ contains
     use kinds
     use iso_c_binding
     use config_mod
-    
+
     implicit none
 
     type(ocean_grid_type),            intent(out) :: G          !< The horizontal grid type (same for ice & ocean)
@@ -105,7 +105,7 @@ contains
     type(param_file_type)                  :: param_file                !< Structure to parse for run-time parameters
     type(directories)                      :: dirs                      !< Structure containing several relevant directory paths
     logical                                :: global_indexing = .false. !< If true use global horizontal index DOES NOT WORK
-    logical                                :: write_geom_files = .false.!< 
+    logical                                :: write_geom_files = .false.!<
     type(ocean_OBC_type),          pointer :: OBC => NULL()             !< Ocean boundary condition
     integer :: NCat_dflt = 5
     integer :: ncat, nkice, nksnow, km
@@ -135,25 +135,25 @@ contains
     ! Destructor for dynamic grid
     call destroy_dyn_horgrid(dG)
     dG => NULL()
-    
+
     ! Initialize sea-ice grid
     ice_column%ncat = config_get_int(c_conf,"num_ice_cat")
     ice_column%nzi = config_get_int(c_conf,"num_ice_lev")
     ice_column%nzs = config_get_int(c_conf,"num_sno_lev")
-    
+
     call close_param_file(param_file)
-    
+
     call fms_io_exit()
 
   end subroutine soca_geom_init
 
   ! ------------------------------------------------------------------------------
-  
+
   subroutine soca_geom_end(G, GV)
 
     use MOM_grid,                  only : MOM_grid_init, MOM_grid_end, ocean_grid_type
-    use MOM_verticalGrid,          only : verticalGrid_type, verticalGridInit, verticalGridEnd    
-    
+    use MOM_verticalGrid,          only : verticalGrid_type, verticalGridInit, verticalGridEnd
+
     implicit none
 
     type(ocean_grid_type),            intent(inout) :: G
@@ -166,9 +166,9 @@ contains
   end subroutine soca_geom_end
 
   ! ------------------------------------------------------------------------------
-  
+
   subroutine soca_field_init(aogcm, G, GV, ice_column)
-    
+
     use MOM_state_initialization, only : MOM_initialize_state
     use MOM_time_manager,         only : time_type, set_time, time_type_to_real, operator(+)
     use MOM_file_parser,           only : get_param, param_file_type
@@ -177,9 +177,9 @@ contains
     use fms_io_mod,                only : fms_io_init, fms_io_exit
     use MOM_file_parser,           only : open_param_file, close_param_file, get_param
     use fms_mod,                   only : read_data
-    
+
     implicit none
-    
+
     type (Coupled),                     intent(out) :: aogcm
     type(ocean_grid_type), intent(inout)           :: G
     type(verticalGrid_type), pointer, intent(inout):: GV
@@ -199,7 +199,7 @@ contains
     isd  = G%isd  ; ied  = G%ied  ; jsd  = G%jsd  ; jed  = G%jed
     IsdB = G%IsdB ; IedB = G%IedB ; JsdB = G%JsdB ; JedB = G%JedB
 
-    ! TODO: Get from config 
+    ! TODO: Get from config
     ncat = ice_column%ncat
     nzi = ice_column%nzi
     nzs = ice_column%nzs
@@ -223,18 +223,18 @@ contains
   end subroutine soca_field_init
 
   ! ------------------------------------------------------------------------------
-  
+
   subroutine soca_field_end(aogcm)
-    
+
     implicit none
-    
+
     type (Coupled),                     intent(inout) :: aogcm
 
     ! Deallocate ocean state
     deallocate(aogcm%Ocn%T)
     deallocate(aogcm%Ocn%S)
     deallocate(aogcm%Ocn%ssh)
-    deallocate(aogcm%Ocn%H)    
+    deallocate(aogcm%Ocn%H)
 
 
     deallocate(aogcm%Ice%part_size)
@@ -246,5 +246,5 @@ contains
     deallocate(aogcm%Ice%T_skin)
 
   end subroutine soca_field_end
-  
+
 end module soca_mom6
