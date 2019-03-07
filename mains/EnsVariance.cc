0dc6064367b21cbb073376545ce4a5890731bcdc
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2019-03-07 14:12:01 -0500

    Manual merge of the olf feature/moment branch.

--- mains/EnsVariance.cc
+++ mains/EnsVariance.cc
@@ -1,16 +1,18 @@
 /*
- * (C) Copyright 2017 UCAR
+ * (C) Copyright 2019 UCAR
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  */
 #include "src/Traits.h"
-#include "oops/runs/Moments.h"
+#include "src/Transforms/instantiateBalanceOpFactory.h"
+#include "oops/runs/EnsVariance.h"
 #include "src/Run/Run.h"
 
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
-  oops::Moments<soca::Traits> moments;  
-  run.execute(moments);
+  soca::instantiateBalanceOpFactory();  
+  oops::EnsVariance<soca::Traits> ensvariance;  
+  run.execute(ensvariance);
   return 0;
 }
