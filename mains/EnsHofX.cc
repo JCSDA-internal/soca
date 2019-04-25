e87180da982feb10d2baf0fc1f4054b03ad78172
Travis sluka <travissluka@gmail.com>
2019-04-25 15:31:29 -0600

    Add Ensmble and 3D hofx apps (#101)
    * add 3d hofx app and tests
    
    * add ensemble hofx app and tests

---
+++ mains/EnsHofX.cc
@@ -0,0 +1,19 @@
+/*
+ * (C) Copyright 2019 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "src/Traits.h"
+#include "oops/runs/EnsHofX.h"
+#include "src/instantiateObsFilterFactory.h"
+#include "src/Run/Run.h"
+
+int main(int argc,  char ** argv) {
+  soca::Run run(argc, argv);
+  soca::instantiateObsFilterFactory();
+  oops::EnsHofX<soca::Traits> hofx;
+  run.execute(hofx);
+  return 0;
+}
