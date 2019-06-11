5ee8a838717fcc7180d576d322182eff09cddd78
Guillaume Vernieres <guillaume.vernieres@gmail.com>
2019-06-11 11:54:59 +0000

    fixed issue in cice5 init script

--- scripts/seaice/soca_cice5_init.py
+++ scripts/seaice/soca_cice5_init.py
@@ -24,7 +24,6 @@ from __future__ import print_function
 import argparse
 import glob
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
 import shutil
 import soca_cice5_utils
@@ -73,55 +72,35 @@ if __name__ == '__main__':
     #===========================
     bkg  = soca_cice5_utils.SeaIceState(fname=bkg_fname, gridname=grid_fname, ocnfname=ocn_fname, descriptor='Background')
     ana  = soca_cice5_utils.SeaIceState(fname=ana_fname, gridname=grid_fname, ocnfname=ocn_fname, descriptor='Analysis')
-    incr = soca_cice5_utils.SeaIceState(gridname=grid_fname, descriptor='Increment')
+    bkg.info()
     ana.info()
-
-    #bkg.hist()
-    #plt.show()
-
-    #incr.delta(ana, bkg)
-    #incr.update_ocn()
-    incr.info()
-
-    cmap=cm.jet
-    #print 'Increment'
-    #raw_input('<>?')
-    #incr.plot(varname='aicen',vmin=-0.1,vmax=0.1,CMAP=cmap)
-    #incr.plot(varname='hicen',vmin=-0.02,vmax=0.02,CMAP=cmap)
-    #incr.plot(varname='hsnon',vmin=-0.01,vmax=0.01,CMAP=cmap)
-    #incr.plot(varname='Tsfcn',vmin=-2.,vmax=2.,CMAP=cmap)
-    #incr.plot(varname='',vmin=-0.2,vmax=0.2,CMAP=cmap)
-    plt.show()    
 
     # Fix bounds for aicen, vicen and Tsfcn in analysis
     #==================================================
     ana.fix_bounds()
+    ana.update()
+    ana.info()
 
     # Define min/max & masking for incrememnt
     #========================================
-    amin = 1e-3
-    amax = 4.0
-    tiny = 1e-3 # Masking parameter
+    amin = 1e-6
+    amax = 10.0
+    tiny = 1e-6 # Masking parameter
 
-    # Check aicen and fix bounds again
-    #=================================
-    for cat in range(bkg.ncat):   
-        alfa=ana.aicen[cat,:,:]/bkg.aicen[cat,:,:]
-        alfa[alfa>amax]  = amax
-        alfa[alfa<amin]  = amin
-        I=np.where(bkg.aicen[cat,:,:]<tiny)
-        alfa[I]=1.0
-        ana.aicen[cat,:,:]=alfa*bkg.aicen[cat,:,:]
-    ana.fix_bounds(mask=True)
-    bkg.info()
+    alfa = np.ones(np.shape(bkg.aice))
+    I=np.where(bkg.aice>tiny)
+    alfa[I]=ana.aice[I]/bkg.aice[I]
+    alfa[alfa>amax]  = amax
+    alfa[alfa<amin]  = amin
+
+    alfa=0.9999999*alfa
+    # Update ice and snow
+    #====================
+    ana.aicen=alfa*bkg.aicen
+    ana.vicen=alfa*bkg.vicen
+    ana.vsnon=alfa*bkg.vsnon
+    ana.update()
     ana.info()
-    
-    # Update ice and snow Volume, conserve background hicen and hsno
-    #===============================================================
-    for cat in range(bkg.ncat):    
-        ana.vicen[cat,:,:]=ana.aicen[cat,:,:]*bkg.hicen[cat,:,:]
-        ana.vsnon[cat,:,:]=ana.aicen[cat,:,:]*bkg.hsnon[cat,:,:]
-    ana.fix_bounds()
 
     # Write analysis in clone of background file
     #===========================================
@@ -129,59 +108,5 @@ if __name__ == '__main__':
     bkg.write(VAR=ana.aicen,fname=output_fname, varname='aicen')
     bkg.write(VAR=ana.vicen,fname=output_fname, varname='vicen')
     bkg.write(VAR=ana.vsnon,fname=output_fname, varname='vsnon')
-    #bkg.write(VAR=ana.tsfcn,fname=output_fname, varname='Tsfcn')
-    #bkg.write(VAR=ana.aicen,fname=output_fname, varname='aicen')
-
-    # Save increment
-    #===============
-    incr.delta(ana, bkg)
-    incr.info()
-
-    incr.plot(varname='aicen',vmin=-0.025,vmax=0.025,figname='aicen.png')
-    #incr.plot(varname='hicen',vmin=-0.02,vmax=0.02,figname='hicen.png')
-    #incr.plot(varname='hsnon',vmin=-0.01,vmax=0.01,figname='hsnon.png')
-    #incr.plot(varname='Tsfcn',vmin=-2.,vmax=2.,figname='Tsfcn.png')
-
-    incr_fname='incr.nc'
-    incr.write(VAR=incr.aicen,fname=incr_fname, varname='aicen',mode='w')
-    incr.write(VAR=incr.vicen,fname=incr_fname, varname='vicen',mode='a')
-    incr.write(VAR=incr.vsnon,fname=incr_fname, varname='vsnon',mode='a')
-    incr.write(VAR=incr.tsfcn,fname=incr_fname, varname='Tsfcn',mode='a')
-    incr.write(VAR=incr.aice,fname=incr_fname,  varname='aice',mode='a',size='2D')
-    incr.write(VAR=incr.hice,fname=incr_fname,  varname='hice',mode='a',size='2D')
-    incr.write(VAR=incr.dT,fname=incr_fname,  varname='dT',mode='a',size='2D')    
-
-    #Save Ocean restart
-    #T=soca_cice5_utils.readvar(ocn_fname,'Temp')
-    #S=soca_cice5_utils.readvar(ocn_fname,'Salt')    
-    #T[0,:,:]=T[0,:,:]+incr.dT
-    
-    #ncfile = netcdf.netcdf_file('ocn_rst_out.nc','w')
-    #ncfile.createDimension('Time',1)
-    #ncfile.createDimension('zaxis_1',40)
-    #ncfile.createDimension('yaxis_1',410)
-    #ncfile.createDimension('xaxis_1',720)    
-    #VARtmp = ncfile.createVariable('Temp',np.dtype('float32').char,('Time','zaxis_1','yaxis_1','xaxis_1'))
-    #VARtmp[0,:,:,:]=T
-    #VARtmp = ncfile.createVariable('Salt',np.dtype('float32').char,('Time','zaxis_1','yaxis_1','xaxis_1'))
-    #VARtmp[0,:,:,:]=S
-    #ncfile.close()
-    
-
-    
-    #I=np.where(bkg.aice<0.1)
-    #AICE=reshape(ana.aicen[cat,I[0],I[1]],
-    #plt.plot(ana.aicen[cat,I[0],I[1]]
-
-    #plt.plot(np.squeeze(ana.aice.flatten()),np.squeeze(np.sum(ana.tsfcn,axis=0).flatten()),'.k')
-    #plt.grid(True)
-    #plt.show()
-
-    #plt.figure(num=1)
-    #plot2d(bkg.x, bkg.y, bkg.aicen, vmin=0.0, vmax=1.1)
-
-    #plt.figure(num=2)
-    #plot2d(ana.x, ana.y, ana.aicen, vmin=0.0, vmax=1.1)
-    #plt.show()
 
 
--- scripts/seaice/soca_cice5_utils.py
+++ scripts/seaice/soca_cice5_utils.py
@@ -11,36 +11,10 @@ TODO:
 from netCDF4 import Dataset
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
 import matplotlib.cm as cm
 import scipy.io.netcdf as netcdf
 import scipy
-from matplotlib.colors import LinearSegmentedColormap
 import soca_cice5_thermo as thermo
-
-class nlcmap(LinearSegmentedColormap):
-    """A nonlinear colormap"""
-    
-    name = 'nlcmap'
-    
-    def __init__(self, cmap, levels):
-        self.cmap = cmap
-        # @MRR: Need to add N for backend
-        self.N = cmap.N
-        self.monochrome = self.cmap.monochrome
-        self.levels = np.asarray(levels, dtype='float64')
-        self._x = self.levels / self.levels.max()
-        self._y = np.linspace(0.0, 1.0, len(self.levels))
-    
-    #@MRR Need to add **kw for 'bytes'
-    def __call__(self, xi, alpha=1.0, **kw):
-        """docstring for fname"""
-        # @MRR: Appears broken? 
-        # It appears something's wrong with the
-        # dimensionality of a calculation intermediate
-        #yi = stineman_interp(xi, self._x, self._y)
-        yi = np.interp(xi, self._x, self._y)
-        return self.cmap(yi, alpha)
 
 def readvar(fname, varname):
     """Function reading netcdf file
@@ -55,7 +29,6 @@ def readvar(fname, varname):
         else:
             VAR (float)
     """
-    print fname,varname
     ncfile = Dataset(fname,'r')
     VAR=np.squeeze(ncfile.variables[varname][:])
     try:
@@ -69,63 +42,6 @@ def readvar(fname, varname):
         return VAR, ni, nj, ncat
     except:
         return VAR
-
-def plot2d(x, y, var, SUM=True, vmin=-0.1, vmax=0.1, CMAP=cm.jet, map=None, pole='N'):
-    """Basic polar stereographic plot
-    
-    Args:
-        x     (2D array float): Longitude array
-        y     (2D array float): Latitude array
-        var (2/3D array float): variable to be ploted
-        
-    Optional Args:
-        SUM   (bool): Aggregate var
-        vmin (float): min value for levels
-        vmax (float): max value for levels
-    """
-    if map==None:
-        #map = Basemap(projection='hammer',lon_0=180)
-        #map = Basemap(projection='ortho',lat_0=45,lon_0=-100,resolution='l')
-        #map = Basemap(projection='ortho',lat_0=-45,lon_0=-100,resolution='l')
-        if pole=='N':
-            map = Basemap(projection='npstere',lon_0=0,boundinglat=50, resolution='l')
-        if pole=='S':
-            map = Basemap(projection='spstere',lon_0=0,boundinglat=-55, resolution='l')
-        if pole=='C':        
-            #map = Basemap(projection='mill',lon_0=180)
-            map = Basemap(projection='hammer',lon_0=200)            
-        map.drawcoastlines()
-        map.fillcontinents(color='coral')
-        map.drawparallels(np.arange(-90.,120.,15.))
-        map.drawmeridians(np.arange(0.,420.,30.))
-        
-    #vmin=max(np.min(var.flatten()),-1e17)
-    #vmax=min(np.max(var.flatten()),1e17)
-
-    print 'vmin=',vmin
-    print 'vmax=',vmax
-    print '===================='
-    #vmin = 0.5*vmin
-    #vmin = 0.0 #-vmax
-    #dl=(vmax-vmin)/25.0
-    #levels=np.arange(vmin, vmax, dl)
-    levels=np.linspace(vmin, vmax, 25, endpoint=True)
-
-    #if SUM:
-    #    VAR=np.sum(var, axis=0)
-    #    VAR[np.abs(VAR)<0.0]=np.nan
-    #    #VAR[VAR>1.0]=np.nan
-    #else:
-    VAR=var*(var/var)
-    #map.contourf(x, y, VAR, levels, origin='lower',cmap=CMAP, extend='both',latlon=True)    
-    try:
-        map.contourf(x, y, VAR, levels, origin='lower',cmap=CMAP, extend='both',latlon=True)
-        plt.colorbar(shrink=0.5)        
-    except:
-        pass
-        
-    #plt.imshow(VAR)
-    return map
 
 class SeaIceState:
     def __init__(self, gridname='geom_output.nc', fname=None, ocnfname=None, descriptor='Sea-ice state'):
@@ -143,19 +59,19 @@ class SeaIceState:
             try:
                 self.iceumask, self.ni, self.nj, self.ncat = readvar(fname, 'iceumask')
             except:
-                print 'No mask'
+                print('No mask')
             if ocnfname:
-                print 'Reading ocean state ...'
+                print('Reading ocean state ...')
                 self.ocnfname=ocnfname
                 self.Tocn = readvar(ocnfname, 'Temp'); self.Tocn=self.Tocn[0,:,:]
                 self.Socn = readvar(ocnfname, 'Salt'); self.Socn=self.Socn[0,:,:]
                 self.Tm = thermo.Tm(self.Socn)
         else:
-            print 'No checkpoint file provided, initializing state to 0'
+            print('No checkpoint file provided, initializing state to 0')
             self.ni=np.shape(self.x)[1]
             self.nj=np.shape(self.x)[0]
             self.ncat=5 # <--- hard coded, need to change that!
-            print 'ni=',self.ni,' nj=',self.nj
+            print('ni=',self.ni,' nj=',self.nj)
             shape=(self.ncat,self.nj,self.ni)
             self.aicen=np.zeros(shape)
             self.vicen=np.zeros(shape)
@@ -179,71 +95,16 @@ class SeaIceState:
         self.tsfc=np.sum(self.aicen*self.tsfcn, axis=0)
         self.iceumask=np.zeros(np.shape(self.aice))
         self.iceumask[self.aice>0.0]=1.0
-        
-    def __add__(self, other):
-        spo=SeaIceState()
-        spo.aicen[:] = self.aicen + other.aicen
-        spo.vicen[:] = self.vicen + other.vicen
-        spo.vsnon[:] = self.vsnon + other.vsnon
-        spo.tsfcn[:] = self.tsfcn + other.tsfcn
-        spo.hicen[:] = self.hicen + other.hicen
-        spo.hsnon[:] = self.hsnon + other.hsnon
-        spo.aice[:] = self.aice + other.aice
-        spo.hice[:] = self.hice + other.hice
-        spo.hsno[:] = self.hsno + other.hsno
-        spo.tsfc[:] = self.tsfc + other.tsfc
-        return spo
 
-    def __sub__(self, other):
-        spo=SeaIceState()
-        spo.aicen[:] = self.aicen - other.aicen
-        spo.vicen[:] = self.vicen - other.vicen
-        spo.vsnon[:] = self.vsnon - other.vsnon
-        spo.tsfcn[:] = self.tsfcn - other.tsfcn
-        spo.hicen[:] = self.hicen - other.hicen
-        spo.hsnon[:] = self.hsnon - other.hsnon
-        spo.aice[:] = self.aice - other.aice
-        spo.hice[:] = self.hice - other.hice
-        spo.hsno[:] = self.hsno - other.hsno
-        spo.tsfc[:] = self.tsfc - other.tsfc
-        return spo
-    
-    def delta(self, ana, bkg):
-        self.aicen[:] = ana.aicen - bkg.aicen
-        self.vicen[:] = ana.vicen - bkg.vicen
-        self.vsnon[:] = ana.vsnon - bkg.vsnon
-        self.tsfcn[:] = ana.tsfcn - bkg.tsfcn
-        self.hicen[:] = ana.hicen - bkg.hicen
-        self.hsnon[:] = ana.hsnon - bkg.hsnon
-        self.aice[:] = ana.aice - bkg.aice
-        self.hice[:] = ana.hice - bkg.hice
-        self.hsno[:] = ana.hsno - bkg.hsno
-        self.tsfc[:] = ana.tsfc - bkg.tsfc
-
-        print np.shape(self.aice)
-        
-        # case daice<0
-        Im=np.where(self.aice<0.0)
-        self.dT=np.zeros(np.shape(bkg.Tm))
-        self.dT[Im]=self.aice[Im]*(-bkg.Tocn[Im]+bkg.Tm[Im])
-
-        # case daice>=0
-        alpha=1.0#
-        Ip=np.where(self.aice>=0.0)
-        self.dT[Ip]=alpha*self.aice[Ip]*(-bkg.Tocn[Ip]+bkg.Tm[Ip])
-        print 'max dT=',np.max(self.dT), ' min dT=',np.min(self.dT)     
-
-        #plt.imshow(self.dT,vmin=-0.15, vmax=0.15)
-        #plt.show()
-        #return self
-        
     def info(self):
-        print '================== ',self.descriptor,' ==================='
+        print('================== ',self.descriptor,' ===================')
+        print('aice_max=',np.max(np.max(np.max(np.sum(self.aicen[:,:,:],axis=0)))))
+        print('tsfcn_sum=',np.sum(self.tsfcn.flatten()))
         for n in range(self.ncat):
-            print 'cat=',str(n+1),' hi_max=',np.max(self.hicen[n,:,:].flatten()), \
-                                  ' hi_min=',np.min(self.hicen[n,:,:].flatten())
-            print '      hsno_max=',np.max(self.hsnon[n,:,:].flatten()), \
-                                  ' hsno_min=',  np.min(self.hsnon[n,:,:].flatten())
+            print('cat=',str(n+1),' hi_max=',np.max(self.hicen[n,:,:].flatten()), \
+                                  ' hi_min=',np.min(self.hicen[n,:,:].flatten()))
+            print('      hsno_max=',np.max(self.hsnon[n,:,:].flatten()), \
+                                  ' hsno_min=',  np.min(self.hsnon[n,:,:].flatten()))
 
     def write(self, VAR, fname, varname, mode='a', size='3D'):        
         ncfile = netcdf.netcdf_file(fname,mode)
@@ -270,6 +131,15 @@ class SeaIceState:
         VARtmp[:]=VAR[:]
         ncfile.close()
         
+    def update(self):
+        self.aice=np.sum(self.aicen, axis=0)
+        self.vice=np.sum(self.vicen, axis=0)
+        self.hicen=self.vicen/self.aicen
+        self.hsnon=self.vicen/self.aicen
+        self.hice=self.vice/self.aice
+        self.hsno=self.vsno/self.aice
+        self.tsfc=np.sum(self.aicen*self.tsfcn, axis=0)
+
     def fix_bounds(self,mask=False):
         # aicen is in [0,1]
         vmin=0.0
@@ -285,28 +155,20 @@ class SeaIceState:
 
         # vsnon>=0
         Imin=np.where(self.vsnon<0.0)
-        print 'filtering vsnon:',Imin
         self.vsnon[Imin]=0.0
 
-        # We don't want (know how!) to create new ice ==> Mask out new ice
-        if mask:
-            for cat in range(self.ncat):
-                self.aicen[cat,:,:]=self.aicen[cat,:,:]*self.iceumask
-                self.vicen[cat,:,:]=self.vicen[cat,:,:]*self.iceumask
-                self.vsnon[cat,:,:]=self.vsnon[cat,:,:]*self.iceumask
-
         # Make sure aggregate of aicen is in [0,1]        
-        Imax=np.where(np.sum(self.aicen, axis=0)>1.)    
-        self.aicen[:,Imax[0],Imax[1]]=self.aicen[:,Imax[0],Imax[1]]/np.sum(self.aicen[:,Imax[0],Imax[1]], axis=0)
+        Imax=np.where(np.sum(self.aicen, axis=0)>1.0)    
+        self.aicen[:,Imax[0],Imax[1]]=self.aicen[:,Imax[0],Imax[1]]/(np.sum(self.aicen[:,Imax[0],Imax[1]], axis=0)+1e-8)
         
         # By definition, surface temperature of snow or ice is in ]-alot, 0]
         self.tsfcn[self.tsfcn>0.0]=0.0
 
         # Update thicknesses
-        I=np.where(self.aicen>1e-3)
-        self.hicen[I] = self.vicen[I]/self.aicen[I]
-        self.hsnon = np.zeros(np.shape(self.aicen))
-        self.hsnon[I] = self.vsnon[I]/self.aicen[I]
+        #I=np.where(self.aicen>1e-3)
+        #self.hicen[I] = self.vicen[I]/self.aicen[I]
+        #self.hsnon = np.zeros(np.shape(self.aicen))
+        #self.hsnon[I] = self.vsnon[I]/self.aicen[I]
 
         # Update aggregates
         self.aice=np.sum(self.aicen, axis=0)
@@ -318,61 +180,3 @@ class SeaIceState:
     def update_ocn(self):
         # Assert we operate on increment
         self.dT=self.aice*(self.Tm-self.To)
-                
-    def hist(self, varname='aicen', type='incr', CMAP=cm.BrBG):
-
-        for n in range(self.ncat):
-            plt.figure(num=n+1)
-            I=np.where( (self.y>55.0) & (self.aicen[n,:,:]>0.0) )
-            #plt.plot(scipy.special.logit(self.aicen[n,I[0],I[1]].flatten()),\
-            #         scipy.special.logit(self.hicen[n,I[0],I[1]].flatten()),'.g', alpha=0.1)
-            nn, bins, patches = plt.hist(scipy.special.logit(self.aicen[n,I[0],I[1]].flatten()),\
-                                     500, normed=1, facecolor='green', alpha=0.75)
-        plt.show()
-
-    def plot(self, varname='aicen', type='incr', CMAP=cm.jet, NLMAP=False, vmin=-0.1, vmax=0.1, figname='fig.png'):
-        if varname=='aicen': var=self.aicen; agg=self.aice
-        if varname=='hicen': var=self.hicen; agg=self.hice
-        if varname=='hsnon': var=self.hsnon; agg=self.hsno
-        if varname=='Tsfcn': var=self.tsfcn; agg=self.tsfc
-        #if varname=='dT': var=self.dT; agg=self.tsfc
-        
-        plt.figure(num=1,figsize=(18,8))
-        for n in range(self.ncat):
-            print 'cat:',n+1,' ',np.shape(var),np.shape(self.x)
-            plt.subplot(2,3,n+1)
-            #vmin=np.min(var[n,:,:].flatten())
-            #vmax=np.max(var[n,:,:].flatten())
-            plot2d(self.x, self.y, var[n,:,:], vmin=vmin, vmax=vmax, CMAP=CMAP)
-            #plt.colorbar(shrink=0.5)
-            plt.title('cat-'+str(n+1))
-        plt.subplot(2,3,n+2)
-        if NLMAP:
-            levels0=np.linspace(vmin, vmax, num=9, endpoint=True)
-            print levels0
-            amp=vmax-vmin
-            levels=amp*np.tanh((levels0-0.5*amp)/amp)
-            print levels
-            cmap_nonlin = nlcmap(CMAP, levels)
-            CMAP = cmap_nonlin
-        print 'agg:',np.shape(agg),np.shape(self.x)
-        #plot2d(self.x, self.y, agg, vmin=vmin, vmax=vmax, CMAP=CMAP)
-        plot2d(self.x, self.y, self.dT, vmin=-0.15, vmax=0.15, CMAP=CMAP)
-        #plt.colorbar(shrink=0.5)
-        #plt.colorbar(spacing='proportional',
-        #             orientation='vertical',
-        #             shrink=0.7)#, format="%.0f",fontsize=28)
-        #cbar.set_label(r"ET [mm/month]", size=10)
-
-
-
-        #plt.title('Aggregate')
-        plt.title('dT')
-        plt.suptitle(varname)
-        plt.savefig(figname)
-        #plt.show()
-        plt.clf()
-
-
-
-
