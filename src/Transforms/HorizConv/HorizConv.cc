90450bbe2d3ce2c6ea238da06b371534937fc979
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2019-07-11 14:18:23 -0400

    added forgotten files from previous commit

---
+++ src/Transforms/HorizConv/HorizConv.cc
@@ -0,0 +1,62 @@
+/*
+ * (C) Copyright 2017-2018  UCAR.
+ * 
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "src/Transforms/HorizConv/HorizConv.h"
+
+#include <ostream>
+#include <string>
+
+#include "oops/util/Logger.h"
+#include "eckit/config/Configuration.h"
+#include "HorizConvFortran.h"
+#include "src/Increment/Increment.h"
+#include "src/State/State.h"
+#include "src/Geometry/Geometry.h"
+
+using oops::Log;
+
+namespace soca {
+  // -----------------------------------------------------------------------------
+  HorizConv::HorizConv(const State & bkg,
+                 const State & traj,
+                 const Geometry & geom,
+                 const eckit::Configuration & conf): traj_(traj) {
+    const eckit::Configuration * configc = &conf;
+    soca_horizconv_setup_f90(keyFtnConfig_, &configc, traj_.fields().toFortran());
+  }
+  // -----------------------------------------------------------------------------
+  HorizConv::~HorizConv() {
+    soca_horizconv_delete_f90(keyFtnConfig_);
+  }
+  // -----------------------------------------------------------------------------
+  void HorizConv::multiply(const Increment & dxa, Increment & dxm) const {
+    // dxm = K dxa
+    soca_horizconv_mult_f90(keyFtnConfig_,
+                         dxa.fields().toFortran(),
+                         dxm.fields().toFortran());
+  }
+  // -----------------------------------------------------------------------------
+  void HorizConv::multiplyInverse(const Increment & dxm, Increment & dxa) const {
+    dxa = dxm;
+  }
+  // -----------------------------------------------------------------------------
+  void HorizConv::multiplyAD(const Increment & dxm, Increment & dxa) const {
+    // dxa = K^T dxm
+    soca_horizconv_mult_f90(keyFtnConfig_,
+                         dxm.fields().toFortran(),
+                         dxa.fields().toFortran());
+  }
+  // -----------------------------------------------------------------------------
+  void HorizConv::multiplyInverseAD(const Increment & dxa, Increment & dxm) const {
+    dxm = dxa;
+  }
+  // -----------------------------------------------------------------------------
+  void HorizConv::print(std::ostream & os) const {
+    os << "SOCA change variable";
+  }
+  // -----------------------------------------------------------------------------
+}  // namespace soca
---
+++ src/Transforms/HorizConv/soca_horizconv.interface.F90
@@ -0,0 +1,77 @@
+
+!
+! (C) Copyright 2017 UCAR
+!
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+!
+
+! ------------------------------------------------------------------------------
+!> Constructor for D (standard deviation of background error)
+subroutine c_soca_horizconv_setup(c_key_self, c_conf, c_key_bkg) &
+     &bind(c,name='soca_horizconv_setup_f90')
+  use iso_c_binding
+  use soca_horizconv_mod
+  use soca_fields_mod_c
+
+  integer(c_int), intent(inout) :: c_key_self   !< The D structure
+  type(c_ptr),       intent(in) :: c_conf       !< The configuration
+  integer(c_int), intent(in)    :: c_key_bkg    !< Background field
+
+  type(soca_field), pointer :: bkg
+  type(soca_horizconv_config), pointer :: self
+
+  call soca_horizconv_registry%init()
+  call soca_horizconv_registry%add(c_key_self)
+  call soca_horizconv_registry%get(c_key_self, self)
+  call soca_field_registry%get(c_key_bkg, bkg)
+
+  call soca_horizconv_setup(c_conf, self, bkg)
+  
+end subroutine c_soca_horizconv_setup
+
+! ------------------------------------------------------------------------------
+!> Destructor for D
+subroutine c_soca_horizconv_delete(c_key_self) bind(c,name='soca_horizconv_delete_f90')
+  use iso_c_binding
+  use soca_horizconv_mod
+  use soca_fields_mod_c
+
+  implicit none
+  integer(c_int), intent(inout) :: c_key_self
+  type(soca_horizconv_config), pointer :: self
+
+  call soca_horizconv_registry%get(c_key_self, self)
+  call soca_horizconv_registry%remove(c_key_self)
+
+end subroutine c_soca_horizconv_delete
+
+! ------------------------------------------------------------------------------
+!> Multiplication forward and adjoint
+subroutine c_soca_horizconv_mult_f90(c_key_self, c_key_a, c_key_m)&
+     &bind(c,name='soca_horizconv_mult_f90')
+  use iso_c_binding
+  use soca_horizconv_mod
+  use soca_fields_mod_c
+  use kinds
+  use soca_kst_mod
+  
+  implicit none
+  integer(c_int), intent(in) :: c_key_a     !<    "   to Increment in
+  integer(c_int), intent(in) :: c_key_m     !<    "   to Increment out 
+  integer(c_int), intent(in) :: c_key_self 
+
+  type(soca_field), pointer :: dxa
+  type(soca_field), pointer :: dxm
+  type(soca_horizconv_config), pointer :: self
+  
+  call soca_field_registry%get(c_key_a,dxa)
+  call soca_field_registry%get(c_key_m,dxm)
+  call soca_horizconv_registry%get(c_key_self,self)  
+
+  !< Computes dxm = D dxa
+  call copy(dxm, dxa)
+  call soca_horizconv_mult(self, dxa, dxm)
+  
+end subroutine c_soca_horizconv_mult_f90
+
