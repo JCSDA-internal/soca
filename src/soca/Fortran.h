948788d3a842c19a6d71173c726998cc056af2e2
guillaumevernieres <guillaume.vernieres@noaa.gov>
2019-07-16 09:35:28 -0400

    Cleanup of Covariance (#153)
    * type bound procedure + moved linked list to interface
    
    * made 2d convol init and apply optional/configurable from yaml
    
    * Fixed config for covariance and adjusted test output.

--- src/soca/Fortran.h
+++ src/soca/Fortran.h
@@ -152,7 +152,8 @@ namespace soca {
     //  Background error
     // -----------------------------------------------------------------------------
     void soca_b_setup_f90(F90bmat &, const eckit::Configuration * const *,
-                          const F90geom &, const F90flds &);
+                          const F90geom &, const F90flds &,
+                          const eckit::Configuration * const *);
     void soca_b_delete_f90(F90bmat &);
     void soca_b_linearize_f90(const F90flds &, const F90geom &);
     void soca_b_mult_f90(const F90bmat &, const F90flds &,
