2367645d961ab72c50aa760603063e66a261e34e
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2019-08-01 10:54:16 -0400

    Addressed Rahul's comments

--- src/soca/Model/Model.cc
+++ src/soca/Model/Model.cc
@@ -34,9 +34,9 @@ namespace soca {
     Log::trace() << "Model vars: " << vars_ << std::endl;
     tstep_ = util::Duration(model.getString("tstep"));
     setup_mom6_ = model.getBool("setup_mom6", true);
+        const eckit::Configuration * configc = &model;
     if (setup_mom6_)
       {
-        const eckit::Configuration * configc = &model;
         soca_setup_f90(&configc, geom_.toFortran(), keyConfig_);
       }
         Log::trace() << "Model created" << std::endl;
