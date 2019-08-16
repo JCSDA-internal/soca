3cf78b5221355b17788388c0ec5eb2a00f20d843
Rahul Mahajan <aerorahul@users.noreply.github.com>
2019-08-16 09:51:44 -0400

    same as previous commit
    Change-Id: If24608ec89f497fce41707d643100907e112e09a

--- test/executables/TestVariableChange.cc
+++ test/executables/TestVariableChange.cc
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2017 UCAR
+ * (C) Copyright 2017-2019 UCAR.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -12,7 +12,7 @@
 
 int main(int argc,  char ** argv) {
   soca::Run run(argc, argv);
-  soca::instantiateBalanceOpFactory();    
+  soca::instantiateBalanceOpFactory();
   test::LinearVariableChange<soca::Traits> tests;
   run.execute(tests);
   return 0;
