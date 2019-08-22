1d8fd32b493888f3b65286fa9944906006e75565
Rahul Mahajan <aerorahul@users.noreply.github.com>
2019-08-22 17:01:39 -0400

    clean up soca::Run
    Change-Id: Iaf3ed586f8962a216f4392b176c40bf468e1b4bc

--- src/soca/Run/Run.cc
+++ src/soca/Run/Run.cc
@@ -11,25 +11,21 @@
 #include "oops/runs/Run.h"
 #include "eckit/config/Configuration.h"
 
-#include "soca/Fortran.h"
-
 namespace soca {
 
 // -----------------------------------------------------------------------------
 
 Run::Run(int argc, char ** argv): oops::Run(argc, argv) {
-  oops::Log::trace() << "Creating Run" << std::endl;
+  oops::Log::trace() << "Creating SOCA Run" << std::endl;
   const eckit::Configuration * conf = &config();
-  soca_setup_f(&conf);
-  oops::Log::trace() << "Run created" << std::endl;
+  oops::Log::trace() << "SOCA Run created" << std::endl;
 }
 
 // -----------------------------------------------------------------------------
 
 Run::~Run() {
-  oops::Log::trace() << "Destructing Run" << std::endl;
-  soca_finalize_f();
-  oops::Log::trace() << "MPI finalized, Run destructed" << std::endl;
+  oops::Log::trace() << "Destroying SOCA Run" << std::endl;
+  oops::Log::trace() << "SOCA Run destroyed" << std::endl;
 }
 
 // -----------------------------------------------------------------------------
