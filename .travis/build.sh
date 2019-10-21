5d48c9abc403c31624c3d99eb6e0f1dc91771249
Travis Sluka <travissluka@gmail.com>
2019-10-21 11:20:10 -0600

    avoid repo download until desired branch known

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,13 +8,8 @@ echo -e "\nzeroing out 'ccache' statistics"
 ccache -z
 
 # for each dependency repo, make install, if needed
-rebuild=0
 for repo in $LIB_REPOS; do
-    echo -e "\n"
-    echo "************************************************************"
-    echo "  $repo"
-    echo "************************************************************"
-    
+    bundle_dir=$cwd/repo.bundle/$repo
     src_dir=$cwd/repo.src/$repo
     build_dir=$cwd/repo.build/$repo
     install_dir=${REPO_CACHE}/$repo
@@ -24,18 +19,13 @@ for repo in $LIB_REPOS; do
     export ${repo^^}_PATH
 
     # do we skip building this repo?
-    ver_match=0
-    if [[ -e $install_dir/build.version ]]; then
-	diff -q $src_dir/build.version $install_dir/build.version && ver_match=1
-    fi
-    [[ $rebuild == 0 && $ver_match == 0 ]] && rebuild=1
-    if [[ $rebuild == 0 ]]; then
-	echo "Skipping build of $repo"
-	continue
-    fi
+    [[ -e $bundle_dir/skip_rebuild ]] && continue
 
     # if we are to build
-    echo "Building $repo ..."
+    echo -e "\n"
+    echo "************************************************************"
+    echo "  $repo"
+    echo "************************************************************"    
     rm -rf $build_dir
     rm -rf $install_dir
     mkdir -p $build_dir
@@ -44,15 +34,15 @@ for repo in $LIB_REPOS; do
     # run ecbuild
     build_opt_var=LIB_BUILD_OPT_${repo^^}
     build_opt=${!build_opt_var}
-    ecbuild $src_dir -DCMAKE_INSTALL_PREFIX=${install_dir} -DCMAKE_BUILD_TYPE=${LIB_BUILD_TYPE} \
+    time ecbuild $src_dir -DCMAKE_INSTALL_PREFIX=${install_dir} -DCMAKE_BUILD_TYPE=${LIB_BUILD_TYPE} \
     	    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DENABLE_TESTS=OFF -DBUILD_TESTING=OFF $build_opt
 
     # build and install
-    make -j4
-    make install
+    time make -j4
+    time make install
 
     # save compilation info
-    cp $src_dir/build.version $install_dir/    
+    cp $bundle_dir/build.version $install_dir/    
 done
 
 
@@ -66,8 +56,8 @@ src_dir=$cwd/repo.src/${MAIN_REPO}
 build_dir=$cwd/repo.build/${MAIN_REPO}
 mkdir -p  $build_dir
 cd $build_dir
-ecbuild $src_dir -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_BUILD_TYPE=${MAIN_BUILD_TYPE} -DENABLE_GPROF=ON
-make -j4
+time ecbuild $src_dir -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_BUILD_TYPE=${MAIN_BUILD_TYPE} -DENABLE_GPROF=ON
+time make -j4
 
 
 # how useful was ccache?
--- .travis/prep.sh
+++ .travis/prep.sh
@@ -7,11 +7,15 @@ cwd=$(pwd)
 rm -rf bundle
 git clone $BUNDLE_URL bundle
 (cd bundle && git checkout $BRANCH || echo "bundle does not have branch $BRANCH, keeping default branch")
-BUNDLE_REPOS=$(grep "ecbuild_bundle(" bundle/CMakeLists.txt | awk '{print $3}')
+bundle_repos=$(grep "ecbuild_bundle(" bundle/CMakeLists.txt | awk '{print $3}')
 
-# prepare the bundle for each repo and checkout the repo source
-rm -rf repo.build 
-for repo in $MAIN_REPO $LIB_REPOS; do
+
+# prepare a separate bundle for each lib repo, determine version,
+# and checkout the repo source if needed
+rm -rf repo.build
+prev_repo=""
+rebuild=0
+for repo in $LIB_REPOS $MAIN_REPO; do
     echo ""
     
     # setup src directory CMakeLists.txt
@@ -19,52 +23,66 @@ for repo in $MAIN_REPO $LIB_REPOS; do
     rm -rf $repo_bundle_dir
     mkdir -p $repo_bundle_dir
     cp $cwd/bundle/CMakeLists.txt $repo_bundle_dir/
-    for r in $BUNDLE_REPOS; do
+    for r in $bundle_repos; do
 	if [[ $repo != $r ]]; then
 	    sed -i "/.* PROJECT $r .*/d" $repo_bundle_dir/CMakeLists.txt
 	fi
     done
-    	
-    # checkout the repos
-    repo_src_dir=repo.src/$repo
-    rm -rf $repo_src_dir
-    url=$(grep "PROJECT $repo" $repo_bundle_dir/CMakeLists.txt | awk '{print $5}' | sed 's|"||g')
-    branch=$(grep "PROJECT $repo" $repo_bundle_dir/CMakeLists.txt | awk '{print $8}')
-    GIT_LFS_SKIP_SMUDGE=1 git clone -b $branch $url $repo_src_dir
-done
 
-# checkout the main repo's branch for each lib, if desired
-echo -e "\nChecking out matching repos for \"$MATCH_REPOS\"..."
-for repo in $MAIN_REPO $MATCH_REPOS; do
-    echo -n "$repo: "
-    cd $cwd/repo.src/$repo
-    git checkout $BRANCH || echo "$repo: bundle does not have branch $BRANCH, keeping default branch"
-done
+    # determine the branch / git hash tag, based on the branch name in the original bundle
+    repo_url=$(grep "PROJECT $repo" $repo_bundle_dir/CMakeLists.txt | awk '{print $5}' | sed 's|"||g')
+    repo_branch=$(grep "PROJECT $repo" $repo_bundle_dir/CMakeLists.txt | awk '{print $8}')
+    remote_branches=$(git ls-remote -q $repo_url)
+    repo_hash=$( echo "$remote_branches" | grep "refs/heads/$repo_branch" | awk '{print $1}')
 
+    echo "Repo:    $repo"
+    echo "URL:     $repo_url"
 
+    # if this repo is listed in $MATCH_REPOS, see if a repo branch the same name as the main test
+    # repo is available, and use that instead
+    domatch=0
+    for r in $MATCH_REPOS; do [[ $r == $repo ]] && domatch=1 ; done
+    if [[ $domatch == 1 ]]; then
+	echo -n "* trying to match main test repo branch... "
+	line=$(echo "$remote_branches" | grep "refs/heads/$BRANCH" || echo "none")
+	if [[ "$line" != "none" ]]; then
+	    echo "match FOUND"
+	    repo_branch=$BRANCH
+	    repo_hash=$(echo "$line" | awk '{print $1}')
+	else
+	    echo "match NOT found"
+	fi
+    fi
+    echo "Branch:  $repo_branch"
+    echo "githash: $repo_hash"
 
-# determine version information for each repo, to later
-# determine if each repo needs to be rebuilt
-# TODO add other version info such as docker hash
-prev_repo=""
-for repo in $LIB_REPOS; do
-    cd $cwd/repo.src/$repo
-    git_hash=$(git rev-parse HEAD)
-    git_branch=$(git symbolic-ref HEAD)    
-    rm -rf build.version
-    echo "git_branch:$git_branch"     >  build.version
-    echo "git_hash:$git_hash"         >> build.version
-    echo "build_type:$LIB_BUILD_TYPE" >> build.version
-    echo "dependency_repo:$prev_repo" >> build.version    
+    # save the version info to a file
+    verfile=$repo_bundle_dir/build.version
+    rm -rf $verfile
+    echo "repo:$repo" > $verfile    
+    echo "git_branch:repo_branch"     >> $verfile
+    echo "git_hash:$repo_hash"        >> $verfile
+    echo "build_type:$LIB_BUILD_TYPE" >> $verfile
+    echo "dependency_repo:$prev_repo" >> $verfile
     prev_repo=$repo
-done
 
-# run git-lfs on a subset of the repos
-echo -e "\nRunning git-lfs..."
-for repo in $LFS_REPOS; do
-    echo -n "$repo: "
-    cd $cwd/repo.src/$repo
-    git lfs fetch
-    git lfs checkout
-done
+    # if the repo cache is empty, or if the version mismatches,
+    # download the repo and mark it to be built
+    vermatch=0
+    verfile_cache=$REPO_CACHE/$repo/build.version
+    [[ -e $verfile_cache ]] && diff -q $verfile $verfile_cache && vermatch=1
+    [[ $rebuild == 0 && $vermatch == 0 ]] && rebuild=1
+    if [[ $rebuild == 1 ]]; then
 
+	# download the repo
+	skip_lfs=1
+	for r in $LFS_REPOS; do [[ $r == $repo ]] && skip_lfs=0 ; done
+	[[ $skip_lfs == 0 ]] && echo "Using git-lfs"
+	repo_src_dir=repo.src/$repo
+	rm -rf $repo_src_dir
+	GIT_LFS_SKIP_SMUDGE=$skip_lfs git clone -b $repo_branch $repo_url $repo_src_dir
+    else
+	touch $repo_bundle_dir/skip_rebuild
+	echo "Skipping rebuild"
+    fi
+done
