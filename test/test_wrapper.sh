43f95a570d77446e4a0fec9c2f63496b732b6f12
Travis Sluka <travissluka@gmail.com>
2020-03-05 09:50:17 -0700

    combine run/compare steps in ctests

---
+++ test/test_wrapper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# run the executable
+echo ""
+echo "==============================================================================="
+echo "Running test executable"
+echo "==============================================================================="
+mpirun -n $MPI_PES $1 $2 testoutput/${COMPARE_TESTNAME}.log
+e=$?
+if [[ $e -gt 0 ]]; then
+    echo -e "Failed to run executable. Error code: $e \n"
+    exit $e
+fi
+
+# run compare, if needed
+if [[ $SKIP_COMPARE == "FALSE" ]]; then
+    echo ""
+    echo "==============================================================================="
+    echo "Running compare script"
+    echo "==============================================================================="
+
+    $COMPARE_SCRIPT testoutput/${COMPARE_TESTNAME}.log testref/${COMPARE_TESTNAME}.test ${COMPARE_TOL_F} ${COMPARE_TOL_I}
+    e=$?
+    if [[ $e -gt 0 ]]; then
+        echo -e "Failed in the COMPARE step. Error code: $e \n"
+        exit $e
+    fi
+    echo -e "PASSED \n"
+fi
