eaaf67c9d5def81a631ed55bc8ace32f191d16be
Yi-Cheng Teng - NOAA <30629225+clouden90@users.noreply.github.com>
2020-04-01 16:47:46 -0400

    Update soca_state.interface.F90
    Preparation for ConvertState App #311

--- src/soca/State/soca_state.interface.F90
+++ src/soca/State/soca_state.interface.F90
@@ -319,7 +319,8 @@ subroutine soca_state_change_resol_c(c_key_fld,c_key_rhs) bind(c,name='soca_stat
     call soca_state_registry%get(c_key_rhs,rhs)
 
     ! TODO implement a proper change of resolution, just copying for now
-    call fld%copy(rhs)
+    call fld%change_resol(rhs)
+    !call fld%copy(rhs)
 
 end subroutine soca_state_change_resol_c
 
