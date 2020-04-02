061609165c1f1d65402d70c0dd02e33bbf85af0b
frolovsa <55715838+frolovsa@users.noreply.github.com>
2020-04-02 09:17:42 -0600

    Feature/letkf horiz localization (#317)
    * added LETKF application
    
    Co-authored-by: Travis Sluka <travissluka@gmail.com>

---
+++ src/mains/LETKF.cc
@@ -0,0 +1,21 @@
+/*
+ * (C) Copyright 2020 UCAR.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "soca/Traits.h"
+#include "ioda/instantiateObsLocFactory.h"
+#include "oops/runs/LETKF.h"
+#include "oops/runs/Run.h"
+#include "ufo/instantiateObsFilterFactory.h"
+
+int main(int argc,  char ** argv) {
+  oops::Run run(argc, argv);
+  ioda::instantiateObsLocFactory<soca::Traits>();
+  ufo::instantiateObsFilterFactory<soca::Traits>();
+  oops::LETKF<soca::Traits> letkf;
+  run.execute(letkf);
+  return 0;
+}
