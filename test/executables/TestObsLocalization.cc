54cd588a946ae8eae0dfef98937de29a351f253d
Travis Sluka <tsluka@ucar.edu>
2021-04-02 11:51:20 -0400

    update ObsLocalization (#550)
    * update for oops/ioda changes
    
    * add obslocalization test

--- test/executables/TestObsLocalization.cc
+++ test/executables/TestObsLocalization.cc
@@ -1,21 +1,20 @@
 /*
- * (C) Copyright 2020 UCAR.
+ * (C) Copyright 2021-2021 UCAR
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
 #include "soca/Traits.h"
+
 #include "ioda/instantiateObsLocFactory.h"
-#include "oops/runs/LocalEnsembleDA.h"
 #include "oops/runs/Run.h"
-#include "ufo/instantiateObsFilterFactory.h"
+#include "test/interface/ObsLocalization.h"
 #include "ufo/ObsTraits.h"
 
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
   ioda::instantiateObsLocFactory<soca::Traits, ufo::ObsTraits>();
-  ufo::instantiateObsFilterFactory<ufo::ObsTraits>();
-  oops::LocalEnsembleDA<soca::Traits, ufo::ObsTraits> letkf;
-  return run.execute(letkf);
+  test::ObsLocalization<soca::Traits, ufo::ObsTraits> tests;
+  return run.execute(tests);
 }
