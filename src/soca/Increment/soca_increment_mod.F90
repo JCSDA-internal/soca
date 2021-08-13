115255f00f79131ab5d241c8f7edb115eaef95de
Benjamin Menetrier <30638301+benjaminmenetrier@users.noreply.github.com>
2021-08-13 21:07:27 +0200

    Update source, yaml and references (#620)
    Co-authored-by: Travis Sluka <tsluka@ucar.edu>

--- src/soca/Increment/soca_increment_mod.F90
+++ src/soca/Increment/soca_increment_mod.F90
@@ -218,7 +218,7 @@ subroutine soca_increment_set_atlas(self, geom, vars, afieldset)
   type(oops_variables),  intent(in)    :: vars
   type(atlas_fieldset),  intent(inout) :: afieldset
 
-  integer :: jvar, i, jz
+  integer :: jvar, i, jz, nz
   logical :: var_found
   character(len=1024) :: fieldname
   type(soca_field), pointer :: field
@@ -230,8 +230,12 @@ subroutine soca_increment_set_atlas(self, geom, vars, afieldset)
       field => self%fields(i)
       if (trim(vars%variable(jvar))==trim(field%name)) then
         if (.not.afieldset%has_field(vars%variable(jvar))) then
+          ! Variable dimension
+          nz = field%nz
+          if (nz==1) nz = 0
+
           ! Create field
-          afield = geom%afunctionspace%create_field(name=vars%variable(jvar),kind=atlas_real(kind_real),levels=field%nz)
+          afield = geom%afunctionspace%create_field(name=vars%variable(jvar),kind=atlas_real(kind_real),levels=nz)
 
           ! Add field
           call afieldset%add(afield)
@@ -257,8 +261,8 @@ subroutine soca_increment_to_atlas(self, geom, vars, afieldset)
   type(oops_variables),  intent(in)    :: vars
   type(atlas_fieldset),  intent(inout) :: afieldset
 
-  integer :: jvar, i, jz
-  real(kind=kind_real), pointer :: real_ptr_2(:,:)
+  integer :: jvar, i, jz, nz
+  real(kind=kind_real), pointer :: real_ptr_1(:), real_ptr_2(:,:)
   logical :: var_found
   character(len=1024) :: fieldname
   type(soca_field), pointer :: field
@@ -269,22 +273,33 @@ subroutine soca_increment_to_atlas(self, geom, vars, afieldset)
     do i=1,size(self%fields)
       field => self%fields(i)
       if (trim(vars%variable(jvar))==trim(field%name)) then
+        ! Variable dimension
+        nz = field%nz
+        if (nz==1) nz = 0
+
         if (afieldset%has_field(vars%variable(jvar))) then
           ! Get field
           afield = afieldset%field(vars%variable(jvar))
         else
           ! Create field
-          afield = geom%afunctionspace%create_field(name=vars%variable(jvar),kind=atlas_real(kind_real),levels=field%nz)
+          afield = geom%afunctionspace%create_field(name=vars%variable(jvar),kind=atlas_real(kind_real),levels=nz)
 
           ! Add field
           call afieldset%add(afield)
         end if
 
         ! Copy data
-        call afield%data(real_ptr_2)
-        do jz=1,field%nz
-          real_ptr_2(jz,:) = pack(field%val(geom%isc:geom%iec,geom%jsc:geom%jec,jz),.true.)
-        end do
+        if (nz==0) then
+          call afield%data(real_ptr_1)
+          real_ptr_1 = reshape(field%val(geom%isc:geom%iec,geom%jsc:geom%jec,1), &
+        & (/(geom%iec-geom%isc+1)*(geom%jec-geom%jsc+1)/))
+        else
+          call afield%data(real_ptr_2)
+          do jz=1,nz
+            real_ptr_2(jz,:) = reshape(field%val(geom%isc:geom%iec,geom%jsc:geom%jec,jz), &
+          & (/(geom%iec-geom%isc+1)*(geom%jec-geom%jsc+1)/))
+          end do
+        end if
 
         ! Release pointer
         call afield%final()
@@ -307,31 +322,40 @@ subroutine soca_increment_from_atlas(self, geom, vars, afieldset)
   type(oops_variables),  intent(in)    :: vars
   type(atlas_fieldset),  intent(in)    :: afieldset
 
-  integer :: jvar, i, jz
-  real(kind=kind_real), pointer :: real_ptr_2(:,:)
-  logical :: umask(geom%isc:geom%iec,geom%jsc:geom%jec),var_found
+  integer :: jvar, i, jz, nz
+  real(kind=kind_real), pointer :: real_ptr_1(:), real_ptr_2(:,:)
+  logical :: var_found
   character(len=1024) :: fieldname
   type(soca_field), pointer :: field
   type(atlas_field) :: afield
 
   ! Initialization
   call self%zeros()
-  umask = .true.
 
   do jvar = 1,vars%nvars()
     var_found = .false.
     do i=1,size(self%fields)
       field => self%fields(i)
       if (trim(vars%variable(jvar))==trim(field%name)) then
+        ! Variable dimension
+        nz = field%nz
+        if (nz==1) nz = 0
+
         ! Get field
         afield = afieldset%field(vars%variable(jvar))
 
         ! Copy data
-        call afield%data(real_ptr_2)
-        do jz=1,field%nz
-          field%val(geom%isc:geom%iec,geom%jsc:geom%jec,jz) = unpack(real_ptr_2(jz,:), &
-          & umask,field%val(geom%isc:geom%iec,geom%jsc:geom%jec,jz))
-        end do
+        if (nz==0) then
+          call afield%data(real_ptr_1)
+          field%val(geom%isc:geom%iec,geom%jsc:geom%jec,1) = reshape(real_ptr_1, &
+        & (/geom%iec-geom%isc+1,geom%jec-geom%jsc+1/))
+        else
+          call afield%data(real_ptr_2)
+          do jz=1,nz
+            field%val(geom%isc:geom%iec,geom%jsc:geom%jec,jz) = reshape(real_ptr_2(jz,:), &
+          & (/geom%iec-geom%isc+1,geom%jec-geom%jsc+1/))
+          end do
+        end if
 
         ! Release pointer
         call afield%final()
