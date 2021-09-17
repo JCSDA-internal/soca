07d839343831003d5fbb388af88e2b1248716474
Travis Sluka <travissluka@gmail.com>
2021-09-17 15:27:11 -0600

    cleanup stuff

--- src/soca/ObsLocalization/ObsLocRossby.h
+++ src/soca/ObsLocalization/ObsLocRossby.h
@@ -18,7 +18,7 @@
 
 #include "oops/generic/gc99.h"
 
-#include "ufo/obslocalization/ObsLocalization.h"
+#include "ufo/obslocalization/ObsLocGC99.h"
 #include "ufo/obslocalization/ObsLocParameters.h"
 
 #include "soca/ObsLocalization/ObsLocRossbyParameters.h"
@@ -28,7 +28,7 @@
 namespace soca {
 
 template<class MODEL>
-class ObsLocRossby: public ufo::ObsLocalization<MODEL> {
+class ObsLocRossby: public ufo::ObsLocGC99<MODEL> {
   typedef typename MODEL::GeometryIterator   GeometryIterator_;
 
  public:
@@ -47,7 +47,7 @@ template<typename MODEL>
 ObsLocRossby<MODEL>::ObsLocRossby(
       const eckit::Configuration& config,
       const ioda::ObsSpace & obsspace):
-    ufo::ObsLocalization<MODEL>::ObsLocalization(config, obsspace) {
+    ufo::ObsLocGC99<MODEL>::ObsLocGC99(config, obsspace) {
   options_.deserialize(config);
 }
 
@@ -70,28 +70,9 @@ void ObsLocRossby<MODEL>::computeLocalization(
   // convert from gaussian to gaspari-cohn width
   lengthscale *= 2.0/sqrt(0.3);
 
-  // do distance search
-  ufo::ObsLocalization<MODEL>::searchObs(i, lengthscale);
-
-  // return refs to internals of ObsLocalization
-  const std::vector<int> & localobs = ufo::ObsLocalization<MODEL>::localobs();
-  const std::vector<double> & horizontalObsdist =
-    ufo::ObsLocalization<MODEL>::horizontalObsdist();
-
-  // set all to missing (outside of localization distance)
-  const double missing = util::missingValue(double());
-  for (size_t jj = 0; jj < locvector.size(); ++jj) {
-    locvector[jj] = missing;
-  }
-
-  // calculate distance based localization
-  const size_t nvars = locvector.nvars();
-  for (size_t jlocal = 0; jlocal < localobs.size(); ++jlocal) {
-    double locFactor = oops::gc99(horizontalObsdist[jlocal] / lengthscale);
-    for (size_t jvar = 0; jvar < nvars; ++jvar) {
-      locvector[jvar + localobs[jlocal] * nvars] = locFactor;
-    }
-  }
+  // do GC99 localization
+  ufo::ObsLocGC99<MODEL>::setLengthscale(lengthscale);
+  ufo::ObsLocGC99<MODEL>::computeLocalization(i, locvector);
 }
 
 // -----------------------------------------------------------------------------
