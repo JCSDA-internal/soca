0ef788cfdf635782e87e6e9f9cd33bd7e5c04d5f
Travis Sluka <tsluka@ucar.edu>
2021-11-12 09:42:10 -0700

    Implement analytic init, and handle upcoming OOPS chvar_tlad PR (#680)
    * analytic init
    
    * analytic init working
    
    * coding norms / soca
    
    * linear get values

--- src/soca/State/State.cc
+++ src/soca/State/State.cc
@@ -38,15 +38,23 @@ namespace soca {
     Log::trace() << "State::State created." << std::endl;
   }
   // -----------------------------------------------------------------------------
-  State::State(const Geometry & geom, const eckit::Configuration & file)
+  State::State(const Geometry & geom, const eckit::Configuration & conf)
     : time_(),
-      vars_(file, "state variables"),
+      vars_(conf, "state variables"),
       geom_(new Geometry(geom))
   {
     util::DateTime * dtp = &time_;
     oops::Variables vars(vars_);
     soca_state_create_f90(keyFlds_, geom_->toFortran(), vars);
-    soca_state_read_file_f90(toFortran(), &file, &dtp);
+
+    if (conf.has("analytic init")) {
+      std::string dt;
+      conf.get("date", dt);
+      time_ = util::DateTime(dt);
+      soca_state_analytic_f90(toFortran(), &conf, &dtp);
+    } else {
+      soca_state_read_file_f90(toFortran(), &conf, &dtp);
+    }
     Log::trace() << "State::State created and read in." << std::endl;
   }
   // -----------------------------------------------------------------------------
