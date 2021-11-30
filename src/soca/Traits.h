d4d571da1e730f6a85cd1870285889cf7ec3d977
danholdaway <danholdaway@users.noreply.github.com>
2021-11-29 22:51:19 -0500

    update trait file

--- src/soca/Traits.h
+++ src/soca/Traits.h
@@ -16,10 +16,12 @@
 #include "soca/GetValues/GetValues.h"
 #include "soca/GetValues/LinearGetValues.h"
 #include "soca/Increment/Increment.h"
+#include "soca/LinearVariableChange/LinearVariableChange.h"
 #include "soca/ModelBias/ModelBias.h"
 #include "soca/ModelBias/ModelBiasCovariance.h"
 #include "soca/ModelBias/ModelBiasIncrement.h"
 #include "soca/State/State.h"
+#include "soca/VariableChange/VariableChange.h"
 
 namespace soca {
 
@@ -33,17 +35,21 @@ struct Traits {
   static std::string name() {return "SOCA";}
   static std::string nameCovar() {return "SocaError";}
 
-  typedef soca::Geometry            Geometry;
-  typedef soca::GeometryIterator    GeometryIterator;
-  typedef soca::State               State;
-  typedef soca::Increment           Increment;
-  typedef soca::ErrorCovariance     Covariance;
-  typedef soca::GetValues           GetValues;
-  typedef soca::LinearGetValues     LinearGetValues;
+  typedef soca::Geometry             Geometry;
+  typedef soca::GeometryIterator     GeometryIterator;
+  typedef soca::State                State;
+  typedef soca::Increment            Increment;
+  typedef soca::ErrorCovariance      Covariance;
+  typedef soca::GetValues            GetValues;
+  typedef soca::LinearGetValues      LinearGetValues;
 
-  typedef soca::ModelBias           ModelAuxControl;
-  typedef soca::ModelBiasIncrement  ModelAuxIncrement;
-  typedef soca::ModelBiasCovariance ModelAuxCovariance;
+  typedef soca::ModelBias            ModelAuxControl;
+  typedef soca::ModelBiasIncrement   ModelAuxIncrement;
+  typedef soca::ModelBiasCovariance  ModelAuxCovariance;
+
+  typedef soca::LinearVariableChange LinearVariableChange;
+  typedef soca::VariableChange       VariableChange;
+
 };
 
 }  // namespace soca
