09a43d8a7b6172e1d3129d0434abfbbf0ea41801
shlyaeva <anna.v.shlyaeva@noaa.gov>
2022-09-26 12:20:19 -0500

    Add bgens parameter for variational task (for use in EnVar)

--- ewok/tasks/variationalMOM6.py
+++ ewok/tasks/variationalMOM6.py
@@ -7,9 +7,9 @@ import os
 from ewok.tasks.variational import variational
 
 class variationalMOM6(variational):
-    def setup(self, config, execs, fix, hcoeffs, statb, bg, obs):
+    def setup(self, config, execs, fix, hcoeffs, statb, bg, bgens, obs):
         # overwrite the executable names
         build = os.environ.get("JEDI_BUILD")
         bindir = os.path.join(build, 'bin')
         execs['anexec'] = os.path.join(bindir, 'soca_var.x')
-        super().setup(config, execs, fix, hcoeffs, statb, bg, obs)
+        super().setup(config, execs, fix, hcoeffs, statb, bg, bgens, obs)
