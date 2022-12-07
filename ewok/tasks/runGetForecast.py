734db0383188e243e66323ef6b417bec13303e0d
Anna Shlyaeva <annash@ucar.edu>
2022-12-07 13:29:09 -0700

    Adapt to ewok changes in passing dates (#826)
    Co-authored-by: shlyaeva <anna.v.shlyaeva@noaa.gov>

--- ewok/tasks/runGetForecast.py
+++ ewok/tasks/runGetForecast.py
@@ -22,16 +22,9 @@ exp_read = conf['experiment']['expid']
 if 'exp_source' in conf:
     exp_read = conf['exp_source']
 
-# Date and step
-date = yamltools.parse_datetime(conf['date'])
-offset = yamltools.parse_timedelta(conf['offset'])
-fcdate = date - offset
-fcstep = yamltools.parse_timedelta(conf['fcstep'])
-vdate = fcdate + fcstep
-
 # Fetch state
 base = conf['experiment']['expid'] + '.fc.'
-sdate = yamltools.jediformat(fcdate) + '.' + yamltools.jediformat(fcstep)
+sdate = conf['fcdate'] + '.' + conf['fcstep']
 filename = base + sdate + '.$(file_type).nc'
 
 # determine the files needed depending on resolution, and if ice is on or off
@@ -48,7 +41,7 @@ fetch(
     type='fc',
     experiment=exp_read,
     resolution=conf['resolution'],
-    date=yamltools.jediformat(fcdate),
+    date=conf['fcdate'],
     step=conf['fcstep'],
     target_file=filename,
     file_format='netcdf',
