de3d3ced5ba7d53afbe32b1f707270aacd0a7ee2
Travis Sluka <tsluka@ucar.edu>
2023-08-17 10:11:21 -0600

    use proper dx dy

--- src/soca/Utils/soca_gaussian_filter.F90
+++ src/soca/Utils/soca_gaussian_filter.F90
@@ -5,6 +5,7 @@
 
 module soca_gaussian_filter_mod
 
+use atlas_module, only: atlas_geometry
 use kinds, only: kind_real
 use fckit_mpi_module, only: fckit_mpi_min, fckit_mpi_max, fckit_mpi_sum
 use logger_mod
@@ -43,7 +44,7 @@ subroutine calc_stats(field, geom, mask, stats)
   type(soca_geom),       intent(in) :: geom
   logical,               intent(in) :: mask(:,:)
   real(kind=kind_real), intent(out) :: stats(3)
-  
+
   real(kind=kind_real) :: l_min, l_max, l_sum, l_count, g_count
 
   l_min =  minval(    field(geom%isc:geom%iec, geom%jsc:geom%jec), &
@@ -58,7 +59,34 @@ subroutine calc_stats(field, geom, mask, stats)
   call geom%f_comm%allreduce(l_max, stats(2), fckit_mpi_max())
   call geom%f_comm%allreduce(l_sum, stats(3), fckit_mpi_sum())
   call geom%f_comm%allreduce(l_count, g_count, fckit_mpi_sum())
-  stats(3) = stats(3) / g_count 
+  stats(3) = stats(3) / g_count
+end subroutine
+
+! ------------------------------------------------------------------------------
+! calculate the grid spacing (dx,dy) based on the given lat lon values
+subroutine calc_dx_dy(geom, dx, dy)
+  class(soca_geom), intent(in) :: geom
+  real(kind=kind_real), allocatable, intent(inout) :: dx(:,:), dy(:,:)
+
+  integer :: i,j
+  type(atlas_geometry) :: atlas_geom
+
+  atlas_geom = atlas_geometry("Earth")
+  do j = geom%jsc, geom%jec
+    do i = geom%isc, geom%iec
+      if (geom%mask2d(i,j) == 0.0) then
+        ! todo remove this?
+        dx(i,j) = 1e10
+        dy(i,j) = 1e10
+        cycle
+      end if
+      dx(i,j) = 0.5 * atlas_geom%distance(geom%lon(i-1, j), geom%lat(i-1,j), geom%lon(i+1,j), geom%lat(i+1,j))
+      dy(i,j) = 0.5 * atlas_geom%distance(geom%lon(i, j-1), geom%lat(i,j-1), geom%lon(i,j+1), geom%lat(i,j+1))
+    end do
+  end do
+
+  call mpp_update_domains(dx, geom%Domain%mpp_domain, complete=.true.)
+  call mpp_update_domains(dy, geom%Domain%mpp_domain, complete=.true.)
 end subroutine
 
 ! ------------------------------------------------------------------------------
@@ -66,57 +94,68 @@ end subroutine
 ! 1. determine the minimum number of iterations required
 ! 2. calculate the weights based on the number of iteraetions, and the hz scales
 subroutine soca_gaussian_filter_init(self, geom, hz_scales)
-  class(soca_gaussian_filter), intent(inout) :: self
-  class(soca_geom),       target, intent(in) :: geom
-  real(kind=kind_real), allocatable, intent(in) :: hz_scales(:,:)
+  class(soca_gaussian_filter),       intent(inout) :: self
+  class(soca_geom),          target, intent(in)    :: geom
+  real(kind=kind_real), allocatable, intent(in)    :: hz_scales(:,:)
 
   logical :: mask(geom%isd:geom%ied,geom%jsd:geom%jed)
-  real(kind=kind_real) :: dx(geom%isd:geom%ied,geom%jsd:geom%jed)
-  real(kind=kind_real) :: stats(3) ! min,max,mean  
+  real(kind=kind_real), allocatable :: dx(:,:)
+  real(kind=kind_real), allocatable :: dy(:,:)
+  real(kind=kind_real) :: stats(3) ! min,max,mean
   real(kind=kind_real) :: r_tmp(geom%isd:geom%ied,geom%jsd:geom%jed)
   integer :: n_min, i, j
   character(len=1024) :: str
 
   call oops_log%debug("soca_gaussian_filter::init ", flush=.true.)
   self%geom => geom
+  mask = geom%mask2d == 1.0
 
-  ! get the grid spacing, and desired filter standard deviation
-  ! TODO, get the correct dx/dy values!    
+  ! calculate the grid spacing
+  ! (note, I *could* just get dx/dy from mom6, but I'm calculating
+  !  it here anyway in the hopes of being more generic someday)
+  allocate(dx(geom%isd:geom%ied,geom%jsd:geom%jed))
+  allocate(dy(geom%isd:geom%ied,geom%jsd:geom%jed))
+  call calc_dx_dy(geom, dx, dy)
+  call calc_stats(dx, geom, mask, stats )
+  write (str, *) "  dx lengths: min=", stats(1), "max=", stats(2), "mean=", stats(3)
+  call oops_log%debug(str)
+  call calc_stats(dy, geom, mask, stats )
+  write (str, *) "  dy lengths: min=", stats(1), "max=", stats(2), "mean=", stats(3)
+  call oops_log%debug(str)
+
+  ! check the horizontal scales
   call calc_stats(hz_scales, geom, mask, stats )
   write (str, *) "  Hz Scales:  min=", stats(1), "max=", stats(2), "mean=", stats(3)
   call oops_log%debug(str)
 
-  dx = sqrt(geom%cell_area)
-  mask = geom%mask2d == 1.0
-  call calc_stats(dx, geom, mask, stats )
-  write (str, *) "  dx lengths: min=", stats(1), "max=", stats(2), "mean=", stats(3)  
-  call oops_log%debug(str)
-
   ! calculate the minimum number of iterations needed
-  ! n_min = ceil( 3/2 * sigma^2 / dx^2 )
+  ! n_min = ceil( 3/2 * sigma^2 / grid_size^2 )
   r_tmp = 0.0
-  where (dx > 0) r_tmp = 3.0/2.0 * hz_scales**2 / dx**2
+  do j=geom%jsd,geom%jed
+    do i=geom%isd,geom%ied
+      if (.not. mask(i,j)) cycle
+      r_tmp(i,j) = 3.0/2.0 * hz_scales(i,j)**2 / min(dx(i,j), dy(i,j)) **2
+    end do
+  end do
   call calc_stats(r_tmp, geom, mask, stats )
   n_min = ceiling(stats(2)) ! use global max
-  write (str, *) "  minimum iterations: ", n_min  
+  write (str, *) "  minimum iterations: ", n_min
   call oops_log%debug(str)
-  self%n_iter = n_min + 2 ! add 1, just for the heck of it
+  self%n_iter = n_min + 2 ! add 1 or 2, just to be safe
 
-  
   ! calculate the weights for the kernel
-  ! (For now, assume hz isotropic)
-  ! w = sigma^2 / (2 * n * dx^2)
+  ! w = sigma^2 / (2 * n * grid_size^2)
   allocate(self%weights_x(-1:1,geom%isd:geom%ied,geom%jsc:geom%jed))
   self%weights_x = 0.0
   do i = self%geom%isd, self%geom%ied
     do j = self%geom%jsd, self%geom%jed
       if (geom%mask2d(i,j) == 0.0) cycle
 
-      if (dx(i,j) == 0.0) cycle ! why am i geting dx of 0.0 in some places??
-      self%weights_x(-1,i,j) = hz_scales(i,j)**2 / (2.0 * self%n_iter * dx(i,j)**2)          
+      ! calculate the weights, first assuming we have 2 valid neighbors
+      self%weights_x(-1,i,j) = hz_scales(i,j)**2 / (2.0 * self%n_iter * dx(i,j)**2)
       self%weights_x(1,i,j) = self%weights_x(-1,i,j)
       self%weights_x(0,i,j) = 1.0 - 2.0 * self%weights_x(-1,i,j)
-      
+
       ! redistribute weights at boundaries
       if(geom%mask2d(i-1, j) == 0.0) then
         self%weights_x(0,i,j) = self%weights_x(0,i,j) + self%weights_x(-1,i,j)
@@ -125,18 +164,18 @@ subroutine soca_gaussian_filter_init(self, geom, hz_scales)
       if(geom%mask2d(i+1, j) == 0.0) then
         self%weights_x(0,i,j) = self%weights_x(0,i,j) + self%weights_x(1,i,j)
         self%weights_x(1,i,j) = 0.0
-      end if      
+      end if
     end do
-  end do  
-  
+  end do
+
   allocate(self%weights_y(-1:1,geom%isd:geom%ied,geom%jsc:geom%jed))
   self%weights_y = 0.0
   do i = self%geom%isd, self%geom%ied
     do j = self%geom%jsd, self%geom%jed
       if (geom%mask2d(i,j) == 0.0) cycle
 
-      if (dx(i,j) == 0.0) cycle ! why am i geting dx of 0.0 in some places??
-      self%weights_y(-1,i,j) = hz_scales(i,j)**2 / (2.0 * self%n_iter * dx(i,j)**2)    
+      ! calculate the weights, first assuming we have 2 valid neighbors
+      self%weights_y(-1,i,j) = hz_scales(i,j)**2 / (2.0 * self%n_iter * dy(i,j)**2)
       self%weights_y(1,i,j) = self%weights_y(-1,i,j)
       self%weights_y(0,i,j) = 1.0 - 2.0 * self%weights_y(-1,i,j)
 
@@ -148,10 +187,10 @@ subroutine soca_gaussian_filter_init(self, geom, hz_scales)
       if(geom%mask2d(i, j+1) == 0.0) then
         self%weights_y(0,i,j) = self%weights_y(0,i,j) + self%weights_y(1,i,j)
         self%weights_y(1,i,j) = 0.0
-      end if   
+      end if
 
     end do
-  end do  
+  end do
 
 
 end subroutine
@@ -173,8 +212,8 @@ subroutine soca_gaussian_filter_mult(self, xin, xout )
 
   xout = xin
 
-  do iter=0, self%n_iter    
-    
+  do iter=0, self%n_iter
+
     ! x direction
     xtmp = xout
     do i = self%geom%isc, self%geom%iec
@@ -183,16 +222,18 @@ subroutine soca_gaussian_filter_mult(self, xin, xout )
         xout(i,j,:) = &
             self%weights_x( 0, i, j) * xtmp(i,   j, :) &
           + self%weights_x(-1, i, j) * xtmp(i-1, j, :) &
-          + self%weights_x(+1, i, j) * xtmp(i+1, j, :)        
+          + self%weights_x(+1, i, j) * xtmp(i+1, j, :)
       end do
     end do
+    ! TODO, in the above code, go into the halo in the y direction by 1 
+    ! so we can skip this first halo exchange    
     call mpp_update_domains(xout, self%geom%Domain%mpp_domain, complete=.true.)
 
     ! y direction
     xtmp = xout
     do i = self%geom%isc, self%geom%iec
       do j = self%geom%jsc, self%geom%jec
-        if (self%geom%mask2d(i,j) == 0.0) cycle              
+        if (self%geom%mask2d(i,j) == 0.0) cycle
         xout(i,j,:) = &
             self%weights_y( 0, i, j) * xtmp(i,   j, :) &
           + self%weights_y(-1, i, j) * xtmp(i, j-1, :) &
