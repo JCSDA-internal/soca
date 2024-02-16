341516b88f651a5f74f7a6fbf6b8c5df82750187
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2024-02-16 14:11:13 -0500

    Update src/soca/MLBalance/MLBalanceParameters.h
    Co-authored-by: Travis Sluka <tsluka@ucar.edu>

--- src/soca/MLBalance/MLBalanceParameters.h
+++ src/soca/MLBalance/MLBalanceParameters.h
@@ -20,7 +20,6 @@ class MLBalanceParameters : public saber::SaberBlockParametersBase {
   OOPS_CONCRETE_PARAMETERS(MLBalanceParameters, saber::SaberBlockParametersBase)
  public:
   oops::RequiredParameter<eckit::LocalConfiguration> mlbalances{"ML Balances", this};
-  oops::RequiredParameter<eckit::LocalConfiguration> geometry{"geometry", this};
   oops::Variables mandatoryActiveVars() const override {
     return oops::Variables({"tocn", "socn", "ssh", "cicen", "hicen", "hsnon"});}
 };
