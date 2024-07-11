77cfa33ec7ef4d3ec30848288745ad5704342cdb
Kriti <bhargavakriti30@gmail.com>
2024-07-11 14:35:22 -0500

    add the ctest to generate coefficients for HTLM

---
+++ src/mains/GenHybridLinearModelCoeffs.cc
@@ -0,0 +1,17 @@
+/*
+ * (C) Copyright 2017-2021 UCAR.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+
+#include "oops/runs/Run.h"
+#include "oops/runs/GenHybridLinearModelCoeffs.h"
+#include "soca/Traits.h"
+
+int main(int argc,  char ** argv) {
+  oops::Run run(argc, argv);
+  oops::GenHybridLinearModelCoeffs<soca::Traits> genHybridLinearModelCoeffs;
+  return run.execute(genHybridLinearModelCoeffs);
+}
