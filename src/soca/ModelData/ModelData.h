26ce53003a3af3ae6457170c5a21196f08f8a6f1
HamidehGMAO <40796921+HamidehGMAO@users.noreply.github.com>
2024-09-26 16:03:42 -0400

    fixed code norm error

--- src/soca/ModelData/ModelData.h
+++ src/soca/ModelData/ModelData.h
@@ -25,15 +25,16 @@ class ModelData : public util::Printable {
  public:
   static const std::string classname() {return "soca::ModelData";}
   static const oops::Variables defaultVariables() {
-    return oops::Variables(std::vector<std::string>({"surface_temperature_where_sea", 
-	    "sea_water_potential_temperature", "sea_water_salinity", "sea_water_cell_thickness", 
-	    "sea_surface_height_above_geoid", "sea_ice_category_area_fraction", "sea_ice_category_thickness",
-	    "Carbon_nitrogen_detritus_concentration", "Particulate_inorganic_carbon", "colored_dissolved_organic_carbon",
-	    "diatom_concentration", "chlorophyte_concentration", "cyano-bacteria_concentration",
-	    "coccolithophore_concentration", "dinoflagellate_concentration", "phaeocystis_concentration",
-	    "ozone_thickness", "water_vapor", "relative_humidity",
-	    "cloud_area_fraction_in_atmosphere_layer", "cloud_liquid_water_path",
-	    "aerosol_optical_thickness", "single_scattering_albedo", "asymmetry_parameter"}));
+    return oops::Variables(std::vector<std::string>({"surface_temperature_where_sea",
+            "sea_water_potential_temperature", "sea_water_salinity", "sea_water_cell_thickness",
+            "sea_surface_height_above_geoid", "sea_ice_category_area_fraction",
+            "sea_ice_category_thickness", "Carbon_nitrogen_detritus_concentration",
+            "Particulate_inorganic_carbon", "colored_dissolved_organic_carbon",
+            "diatom_concentration", "chlorophyte_concentration", "cyano-bacteria_concentration",
+            "coccolithophore_concentration", "dinoflagellate_concentration",
+            "phaeocystis_concentration", "ozone_thickness", "water_vapor", "relative_humidity",
+            "cloud_area_fraction_in_atmosphere_layer", "cloud_liquid_water_path",
+            "aerosol_optical_thickness", "single_scattering_albedo", "asymmetry_parameter"}));
   }
 
   explicit ModelData(const Geometry &) {}
