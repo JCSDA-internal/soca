4d7988f2c169763ea296ddf330d5c38a80649d08
Travis Sluka <travissluka@gmail.com>
2025-01-24 13:11:53 -0700

    remove unused imports in soca_state_mod module

--- src/soca/State/soca_state_mod.F90
+++ src/soca/State/soca_state_mod.F90
@@ -6,19 +6,11 @@
 !> State fields
 module soca_state_mod
 
-use logger_mod
-use kinds, only: kind_real
-use oops_variables_mod
-use atlas_module, only: atlas_field
-
 ! soca modules
-use soca_geom_mod
 use soca_fields_mod
-use soca_increment_mod
 
 implicit none
 private
-
 
 !-------------------------------------------------------------------------------
 !> State fields.
