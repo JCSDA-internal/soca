caa00bc1021ce26644d942789dd16134e1306614
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2025-02-23 17:46:46 -0500

    wip

--- src/mains/SetCorScales.h
+++ src/mains/SetCorScales.h
@@ -23,6 +23,7 @@
 
 #include "eckit/config/LocalConfiguration.h"
 #include "oops/base/PostProcessor.h"
+#include "oops/generic/gc99.h"
 #include "oops/base/Variables.h"
 #include "oops/mpi/mpi.h"
 #include "oops/runs/Application.h"
@@ -53,16 +54,39 @@ namespace soca {
       rh.horiz_scales(scalesConfig);
 
       // locate islands
-      if (scalesConfig.has("islands")) {
+      if (fullConfig.has("islands")) {
+        oops::Log::info() << "====================== apply explicit island filtering" << std::endl;
         // Get the field set from rh
         atlas::FieldSet rhFs;
         rh.toFieldSet(rhFs);
         // Get the 2D grid
         auto lonlat = atlas::array::make_view<double, 2>(resol.functionSpace().lonlat());
-        const eckit::LocalConfiguration islandsConfig(scalesConfig, "islands");
+        const eckit::LocalConfiguration islandsConfig(fullConfig, "islands");
         // Get the location of Islands from the config
-        const atlas::PointLonLat p0(islandsConfig.getDouble("lon"), islandsConfig.getDouble("lat"));
-      }
+        std::vector<atlas::PointLonLat> p0;
+        const std::vector<double> lons = islandsConfig.getDoubleVector("lon");
+        const std::vector<double> lats = islandsConfig.getDoubleVector("lat");
+        ASSERT(lons.size() == lats.size());
+        for (size_t i = 0; i < lons.size(); ++i) {
+            p0.push_back(atlas::PointLonLat(lons[i], lats[i]));
+        }
+        // Recompute the correlation scales
+        float scale = islandsConfig.getFloat("scale");
+        for (auto point : p0) {
+          oops::Log::info() << "---------- Island location: " << point << std::endl;
+          for (auto & field : rhFs) {
+            oops::Log::info() << "---------- Field name: " << field.name() << std::endl;
+            auto view = atlas::array::make_view<double, 2>(field);
+              for (int jnode = 0; jnode < field.shape(0); ++jnode) {
+                atlas::PointLonLat p1(lonlat(jnode, 0), lonlat(jnode, 1));
+                double d = atlas::util::Earth::distance(point, p1)/1000.0;
+                for (int jlevel = 0; jlevel < field.shape(1); ++jlevel) {
+                  view(jnode, jlevel) *= (1.0 - oops::gc99(d/scale));
+                }
+              }
+            }
+          }
+        }
 
       const eckit::LocalConfiguration rhoutputConfig(fullConfig, "rh output");
       rh.write(rhoutputConfig);
