df77a2dd3682d294e6afbc9ca26d905852609131
Yannick Trémolet <tremolet@ucar.edu>
2025-10-30 10:47:55 -0600

    ObsLocalization doesn't inherit from oops

--- src/soca/ObsLocalization/ObsLocRossby.cc
+++ src/soca/ObsLocalization/ObsLocRossby.cc
@@ -6,15 +6,64 @@
  */
 
 #include "soca/ObsLocalization/ObsLocRossby.h"
-#include "soca/Traits.h"
 
-#include "oops/base/ObsLocalizationBase.h"
-#include "ufo/ObsTraits.h"
+#include <algorithm>
 
+#include "eckit/config/Configuration.h"
+
+#include "ioda/ObsSpace.h"
+#include "ioda/ObsVector.h"
+
+#include "soca/GeometryIterator/GeometryIterator.h"
+#include "soca/ObsLocalization/ObsLocRossbyParameters.h"
+#include "ufo/obslocalization/ObsHorLocGC99.h"
+#include "ufo/obslocalization/ObsLocalizationBase.h"
 
 namespace soca {
 
-static oops::ObsLocalizationMaker< soca::Traits, ufo::ObsTraits,
-    soca::ObsLocRossby<soca::Traits>> obslocrossby_("Rossby");
+// -----------------------------------------------------------------------------
+static ufo::ObsLocalizationMaker<GeometryIterator, soca::ObsLocRossby> obslocrossby_("Rossby");
+// -----------------------------------------------------------------------------
+
+ObsLocRossby::ObsLocRossby(const eckit::Configuration & config,
+                           ioda::ObsSpace & obsspace):
+    ufo::ObsHorLocGC99<GeometryIterator>::ObsHorLocGC99(config, obsspace),
+    options_(), cacheVector_(obsspace), cachePoint_(-999, -999) {
+    options_.validateAndDeserialize(config);
+}
+
+// -----------------------------------------------------------------------------
+
+void ObsLocRossby::computeLocalization(
+    const GeometryIterator & i,
+    ioda::ObsVector & locvector) const {
+  const eckit::geometry::Point3 refPoint = *i;
+  const eckit::geometry::Point2 refPoint2(refPoint[0], refPoint[1]);
+  if (refPoint2 == cachePoint_) {
+    locvector = cacheVector_;
+    return;
+  }
+
+  // calculate the length scale at this location
+  double lengthscale = options_.base;
+  lengthscale += options_.mult * i.getFieldValue("rossby_radius");
+  lengthscale = std::max(lengthscale, options_.min_grid * sqrt(i.getFieldValue("area")));
+  const boost::optional<double> & minval = options_.min;
+  const boost::optional<double> & maxval = options_.max;
+  if (minval != boost::none) lengthscale = std::max(lengthscale, *minval);
+  if (maxval != boost::none) lengthscale = std::min(lengthscale, *maxval);
+
+  // convert from gaussian to gaspari-cohn width
+  lengthscale *= 2.0/sqrt(0.3);
+
+  // Apply GC99 localization
+  const LocalObs_ & localobs =
+  ufo::ObsHorLocGC99<GeometryIterator>::getLocalObs(i, lengthscale);
+  ufo::ObsHorLocGC99<GeometryIterator>::localizeLocalObs(i, locvector, localobs);
+  cacheVector_ = locvector;
+  cachePoint_ = refPoint2;
+}
+
+// -----------------------------------------------------------------------------
 
 }  // namespace soca
